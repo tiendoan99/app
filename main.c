0c6ba27e1afaef5a3eb2a190e638f6b87fe41286
Selfie <minhtien230299@gmail.com>
2019-04-11 07:38:03 +0100

    testTone function started

--- main.c
+++ main.c
@@ -5,7 +5,30 @@
 #include "sound.h"
 #include <signal.h>
 #include "comm.h"
-int main(){
+int main(int argc, char **argv){
+	if(argc>1){		// if the user has given some command line argument
+		printf("Test tone generator\n");
+		int fR, fL, ch;
+		float duration;
+		printf("No. of channels (1 or 2): ");
+		scanf("%d", &ch);
+		if(ch == 1){
+			printf("Mono frequency: ");
+			scanf("%d", &fL); 
+		}
+		else(ch == 2){
+			printf("Give me LEFT and RIGHT freq: ");
+			scanf("%d %d", &fL, &fR);
+		}
+		else{
+			printf("Wrong number of channels!\n");
+			return 1;
+		}
+		printf("Duration of sound: ");
+		scanf("&f", &duration);
+		testTone(ch, fL, fR, duration);
+		return 0;
+	}
 	FILE *f;
 	short sd[RATE];
 	for(;;){
